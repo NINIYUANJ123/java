7bf49b78bd7fcc549759bce3a953b0e18f619123
NINIYUANJ123 <1360222948@qq.com>
2019-05-11 22:41:13 +0800

    java

---
+++ MyMath.java
@@ -0,0 +1,10 @@
+package cd.mldn.demo;
+
+public class MyMath {
+    public static int add(int x,int y){
+        return x+y;
+    }
+    public static int div(int x,int y){
+        return x/y;
+    }
+}
---
+++ demo/TestDemo.java
@@ -0,0 +1,10 @@
+package cd.mldn.demo;
+import static cd.mldn.demo.MyMath.*;
+public class TestDemo {
+    public static void main(String[] args) {
+        /*System.out.println("加法操作:"+MyMath.add(10,20));
+        System.out.println("除法操作"+MyMath.div(10,2));*/
+        System.out.println("加法操作"+add(10,20));
+        System.out.println("除法操作"+div(40,20));
+    }
+}
