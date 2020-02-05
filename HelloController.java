e2c82cbf5da7b16780db8467e1111661039caf16
袁佳伟 <46244355+NINIYUANJ123@users.noreply.github.com>
2020-02-05 13:34:47 +0800

    Add files via upload

--- HelloController.java
+++ HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package org.sang.shiro;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(){
-        int i = 1/0;
-        return "hello";
+    public String hello() {
+        return "hello shiro!";
     }
 }
